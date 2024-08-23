f2fff6bb9a7b76aa46cc36d98311ede548f1a89a
Allysson Carlos <allyssoncarlos.ac21@gmail.com>
2024-08-23 19:22:15 -0300

    Adicionar arquivos de projeto.

---
+++ ConstrutorNovamente/FuncionarioEmpresa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutorNovamente
+{
+    internal class FuncionarioEmpresa
+    {
+        private int matricula;
+        private string nome;
+        public FuncionarioEmpresa()
+        {
+            matricula = 00;
+            nome = "Nome";
+        }
+        public FuncionarioEmpresa(int matricula, string nome)
+        {
+            this.matricula = matricula;
+            this.nome = nome;
+        }
+        public void setMatricula(int matricula)
+        {
+            this.matricula = matricula;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public int getMatricula()
+        {
+            return this.matricula;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void ImprimeValores()
+        {
+            Console.WriteLine("Nome do Funcionario: " + nome);
+            Console.WriteLine("Matricula do Funcionario: " + matricula);
+        }
+    }
+}
---
+++ ConstrutorNovamente/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConstrutorNovamente;
+
+FuncionarioEmpresa f1 = new FuncionarioEmpresa();
+f1.ImprimeValores();
+FuncionarioEmpresa f2 = new FuncionarioEmpresa(35, "Joao Paulo ");
+f2.ImprimeValores();
+Console.ReadKey();
